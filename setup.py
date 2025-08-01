78534c2246482ff0eae370c818698c2532c5c9e7
genie360s <alexgmkwizu@gmail.com>
2025-08-01 13:03:43 +0300

    update: name of package , due to an existing package of the same name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pdforensic',
+    name='pdforensic-authentic-check',
     version='0.1.0',
     description='A simple PDF forensic toolkit using pdfresurrect and bash utilities',
     author='Alex Mkwizu',
